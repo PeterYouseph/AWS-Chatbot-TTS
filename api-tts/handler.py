e4bc362b539757a62f2f7bb70b27674dcc3055d3
peteryouseph <peteryouseph@gmail.com>
2024-06-07 14:55:21 -0400

    docs: adiciona comentários explicativos às functions do handler.py

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -1,21 +1,22 @@
+import os
 import json
 
-
+# Função para verificar a saúde da API TTS ( Se está funcionando corretamente )
 def health(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "message": "Go Serverless v3.0! Your function executed successfully!", # Mensagem de sucesso da requisição teste
         "input": event,
     }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    response = {"statusCode": 200, "body": json.dumps(body)} # Resposta da requisição
 
     return response
-
-def v1_description(event, context):
+# Função para retornar uma breve descrição da API TTS
+def v1_description(event, context): 
     body = {
-        "message": "TTS api version 1."
+        "message": "TTS api version 1." # Descrição da API TTS
     }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
-    return response
+    response = {"statusCode": 200, "body": json.dumps(body)} # Resposta da requisição
+ 
+    return response 
