cb615df8f2dded0e0b6ed4780bc8e9d2e0c4b614
peteryouseph <peteryouseph@gmail.com>
2024-06-12 16:35:56 -0400

    refactor: atualiza nome dos services chamados e inclui comentários no controller.py
    Co-authored-by: Rafinha <rafael-torres-nantes@users.noreply.github.com>

--- api-tts/controller/controller.py
+++ api-tts/controller/controller.py
@@ -1,25 +1,27 @@
 import os
 from dotenv import load_dotenv
-from services.logsDynamodb import DynamoDBClass
+from services.logsDynamoDBService import DynamoDBClass
 from services.s3BucketService import S3BucketClass
-from services.textToSpeech import TTSClass
+from services.textToSpeechService import TTSClass
 
+# Carrega os serviços do DynamoDB, S3 e Polly para serem utilizados
 def load_services():
-    load_dotenv()
+    load_dotenv() # Carrega as variáveis de ambiente do arquivo .env
 
-    DYNAMODB_TABLE_NAME = os.getenv('DYNAMODB_TABLE')
-    S3_BUCKET_NAME = os.getenv('BUCKET_NAME')
+    DYNAMODB_TABLE_NAME = os.getenv('DYNAMODB_TABLE') # Obtém o nome da tabela do DynamoDB
+    S3_BUCKET_NAME = os.getenv('BUCKET_NAME') # Obtém o nome do bucket do S3
+ 
+    logDynamo = DynamoDBClass(DYNAMODB_TABLE_NAME) # Instancia a classe do DynamoDB
+    logDynamo.create_table_dynamodb() # Cria a tabela no DynamoDB
 
-    logDynamo = DynamoDBClass(DYNAMODB_TABLE_NAME)
-    logDynamo.create_table_dynamodb()
+    s3Bucket = S3BucketClass(S3_BUCKET_NAME) # Instancia a classe do S3
+    s3Bucket.create_s3_bucket() # Cria o bucket no S3
 
-    s3Bucket = S3BucketClass(S3_BUCKET_NAME)
-    s3Bucket.create_s3_bucket()
+    tts = TTSClass() # Instancia a classe do Polly
 
-    tts = TTSClass()
+    return logDynamo, s3Bucket, tts # Retorna as instâncias dos serviços
 
-    return logDynamo, s3Bucket, tts
-
+# Função para executar o Polly e salvar o arquivo de áudio
 def run_tts(tts, phrase):
     tts.textToSpeech(phrase)
     tts.saveMP3File()
