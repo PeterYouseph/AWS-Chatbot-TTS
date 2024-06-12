0c5374effd344423edc7a1c118b5510f32f23c6e
peteryouseph <peteryouseph@gmail.com>
2024-06-12 16:23:43 -0400

    refact: inclui comentários no serviço do DynamoDB para melhorar documentação
    Co-authored-by: Rafinha <rafael-torres-nantes@users.noreply.github.com>

--- api-tts/services/logsDynamoDBService.py
+++ api-tts/services/logsDynamoDBService.py
@@ -1,31 +1,18 @@
 import boto3
 from datetime import datetime
-from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import BotoCoreError, ClientError
-from services.importAWSCredentials import aws_credentials
 from uuid import uuid4
 
-# Classe para manipular o DynamoDB e salvar os logs
-class DynamoDBClass: 
-    # Construtor da classe que recebe o nome da tabela do DynamoDB
+# Classe para manipular o DynamoDB e inserir os logs de frases convertidas
+class DynamoDBClass:
     def __init__(self, dynamodb_table_name):
         self.dynamodb_table_name = dynamodb_table_name
-        self.session = self.create_session()
-        print(f"AWS Session: {self.session}")
-        self.dynamodb = self.session.resource('dynamodb', region_name='us-east-1')
-        self.dynamodb_client = self.session.client('dynamodb', region_name='us-east-1')
-    # Método para criar a sessão do DynamoDB
-    def create_session(self):
-        ACCESS_KEY, SECRET_KEY, SESSION_TOKEN = aws_credentials()
-        session = boto3.Session(
-            aws_access_key_id=ACCESS_KEY, 
-            aws_secret_access_key=SECRET_KEY,
-            aws_session_token=SESSION_TOKEN
-        )
-        return session
-    # Método para criar a tabela no DynamoDB
+        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        self.dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+    
+    # Método para criar a tabela no DynamoDB caso ela não exista
     def create_table_dynamodb(self):
-        try:
+        try: # Verifica se a tabela já existe, caso não exista, cria a tabela
             tables = self.dynamodb_client.list_tables()
             if self.dynamodb_table_name not in tables['TableNames']:
                 table = self.dynamodb.create_table(
@@ -42,22 +29,24 @@ class DynamoDBClass:
                     }
                 )
                 table.meta.client.get_waiter('table_exists').wait(TableName=self.dynamodb_table_name)
-        except (BotoCoreError, ClientError) as e:
+        except (BotoCoreError, ClientError) as e: # Caso ocorra um erro, imprime a mensagem de erro
             print(f'Error: {e}')
-    # Método para salvar o log no DynamoDB com o id, a frase e a url do audio
+
+    # Método para inserir os logs no DynamoDB com os dados da frase convertida
     def log_register_dynamodb(self, unique_id, phrase, s3_url):
-        table = self.dynamodb.Table(self.dynamodb_table_name)
+        table = self.dynamodb.Table(self.dynamodb_table_name) 
         log_item = {
             'id': unique_id,
             'timestamp': datetime.utcnow().isoformat(),
             'phrase': phrase,
             'url_to_audio': s3_url
         }
-        try:
+        try: # Insere os dados no DynamoDB
             table.put_item(Item=log_item)
-        except ClientError as e:
+        except ClientError as e: # Caso ocorra um erro, imprime a mensagem de erro
             print(f"Erro ao inserir os dados do log no DynamoDB: {e}")
-    # Método para verificar se a frase já foi salva no DynamoDB
+
+    # Método para verificar se a frase já foi convertida
     def repeated_value_dynamodb(self, unique_id):
         table = self.dynamodb.Table(self.dynamodb_table_name)
         try:
@@ -66,12 +55,13 @@ class DynamoDBClass:
         except ClientError as e:
             print(f"Erro ao buscar a frase no DynamoDB: {e}")
             return None
-    # Método para buscar o item no DynamoDB
+
+    # Método para buscar o item no DynamoDB pelo ID
     def get_item(self, unique_id):
         table = self.dynamodb.Table(self.dynamodb_table_name)
-        try:
+        try: # Busca o item no DynamoDB pelo ID
             response = table.get_item(Key={'id': unique_id})
             return response.get('Item', {})
-        except ClientError as e:
+        except ClientError as e: # Caso ocorra um erro, retorna None
             print(f"Erro ao buscar o item no DynamoDB: {e}")
             return None
