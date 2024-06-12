9393800ad516e969c08de963baf503d0851c956a
peteryouseph <peteryouseph@gmail.com>
2024-06-12 16:27:50 -0400

    refactor: altera nome do service de configuraçaõ do bucket S3 e inclui comentários
    Co-authored-by: Rafinha <rafael-torres-nantes@users.noreply.github.com>

--- api-tts/services/s3BucketService.py
+++ api-tts/services/s3BucketService.py
@@ -1,38 +1,39 @@
-import os
 import boto3
 import logging
-from dotenv import load_dotenv
 from botocore.exceptions import ClientError
-from services.importAWSCredentials import aws_credentials
 
-class S3BucketClass: 
+# Classe para manipular o S3 e fazer o upload dos arquivos de áudio gerados
+class S3BucketClass:
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
-        self.session = self.create_session()
-        self.s3_client = self.session.client('s3')
-         
-    def create_session(self):
-        ACCESS_KEY, SECRET_KEY, SESSION_TOKEN = aws_credentials()
-        session = boto3.Session(
-            aws_access_key_id=ACCESS_KEY, 
-            aws_secret_access_key=SECRET_KEY,
-            aws_session_token=SESSION_TOKEN
-        )
-        return session
-
+        self.s3_client = boto3.client('s3')
+    
+    # Método para criar o bucket no S3 caso ele não exista
     def create_s3_bucket(self):
-        try:
+        try: # Verifica se o bucket já existe, caso não exista, cria o bucket
+            if self._bucket_exists(self.bucket_name):
+                print(f"Bucket {self.bucket_name} já existe.")
+                return True
             self.s3_client.create_bucket(Bucket=self.bucket_name)
-        except ClientError as e:
+        except ClientError as e: # Caso ocorra um erro, imprime a mensagem de erro
             logging.error(e)
             return False
         return True
 
+    # Método para verificar se o bucket já existe no S3
+    def _bucket_exists(self, bucket_name):
+        try: # Verifica se o bucket já existe no S3 e retorna True, caso exista
+            self.s3_client.head_bucket(Bucket=bucket_name)
+            return True
+        except ClientError: # Caso não exista, retorna False 
+            return False 
+
+    # Método para fazer o upload do arquivo no S3 e retornar a URL do arquivo
     def upload_s3_bucket(self, upload_file, filename):
-        try:
+        try: # Faz o upload do arquivo no S3 e retorna a URL do arquivo
             self.s3_client.upload_file(upload_file, self.bucket_name, filename)
             file_url = f"https://{self.bucket_name}.s3.amazonaws.com/{filename}"
             return file_url
-        except ClientError as e:
+        except ClientError as e: # Caso ocorra um erro, imprime a mensagem de erro
             logging.error(e)
             return None
