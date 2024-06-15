a7bdbd6e14ad8c16ea194136220af4e861ad8c2e
peteryouseph <peteryouseph@gmail.com>
2024-06-14 21:38:30 -0400

    refact: adiciona mensagens de erro mais descritivas e verificação de erro no handler.py
    Co-authored-by: Rafinha <rafael-torres-nantes@users.noreply.github.com>

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -22,28 +22,35 @@ def v1_description(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "TTS api version 1.",
+            "message": "TTS api version 1 - Text to Speech API",
         }),
     }
 
 # Function responsável por receber a frase e retornar o áudio gerado pelo Polly e salvo no S3
 def tts(event, context):
     print("Event received:", event)  # Evento recebido pelo Lambda
-    try: # Tenta obter a frase do corpo do evento recebido
+    try:  # Tenta obter a frase do corpo do evento recebido
         body = json.loads(event['body'])
         phrase = body['phrase']
-    except KeyError: # Caso a chave 'body' não seja encontrada no evento, retorna um erro
+    except KeyError:  # Caso a chave 'body' não seja encontrada no evento, retorna um erro
         return {
             "statusCode": 400,
             "body": json.dumps({
-                "message": "Invalid input, 'body' key not found in the event."
+                "message": "Input body not found. - Use {\"phrase\": \"your_text_here\"}"
             }),
         }
-    except json.JSONDecodeError: # Caso o JSON esteja em um formato inválido, retorna um erro
+    except json.JSONDecodeError:  # Caso o JSON esteja em um formato inválido, retorna um erro
         return {
             "statusCode": 400,
             "body": json.dumps({
-                "message": "Invalid JSON format in the body."
+                "message": "Invalid JSON format in the body. - Use {\"phrase\": \"your_text_here\"}"
+            }),
+        }
+    except Exception as e:  # Caso ocorra um erro inesperado, retorna um erro
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": f"Unexpected error: {e}"
             }),
         }
 
@@ -55,15 +62,23 @@ def tts(event, context):
     # Certifica-se de que a frase não foi convertida anteriormente e retorna o áudio gerado caso já tenha sido convertida
     if logDynamo.repeated_value_dynamodb(unique_id):
         item = logDynamo.get_item(unique_id)
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "received_phrase": phrase,
-                "url_to_audio": item['url_to_audio'],
-                "created_audio": item['created_audio'],
-                "unique_id": unique_id
-            }),
-        }
+        if item:
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "received_phrase": phrase,
+                    "url_to_audio": item.get('url_to_audio', 'N/A'),
+                    "created_audio": item.get('created_audio', 'N/A'),
+                    "unique_id": unique_id
+                }),
+            }
+        else:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({
+                    "message": "Error fetching item from DynamoDB. Please try again."
+                }),
+            }
 
     # Converte a frase em áudio
     run_tts(tts, phrase)
